463b9d8a2ffd1e2e6ac6ed5392cd1939837d4209
simetrii <100915992+simetrii@users.noreply.github.com>
2022-03-04 14:28:34 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.1_0"
+export DT_LINK="https://github.com/OrangeFoxRecovery/device_xiaomi_garden -b fox_9.0"
 
-export DEVICE="beryllium"
+export DEVICE="cattail"
 export OEM="xiaomi"
 export TARGET="recoveryimage"
 
